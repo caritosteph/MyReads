cb8a66426bfe072b623ae3c55868f3cdf00e0b13
Carolyn <carito.ulfe@gmail.com>
2018-02-07 00:42:46 +0100

    First commit structure

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import * as BooksAPI from './BooksAPI'
+import './App.css'
+
+class BooksApp extends React.Component {
+
+  render() {
+    return (
+      <div className="app">
+        BookApp
+      </div>
+    )
+  }
+}
+
+export default BooksApp
